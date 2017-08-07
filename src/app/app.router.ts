f081f95e0e7d0abc0aa042eee742f895671ddf49
domd0101 <ddecicco@buffalo.edu>
2017-08-06 19:30:29 -0600

    some routing stuff

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from "@angular/router";
 
 
 import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component"
 
 
 export const router: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'login', component: LoginComponent},
     { path: 'home', component: HomeComponent }
 ]
 
