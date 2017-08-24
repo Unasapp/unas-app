55892fdcb94bd0f77f61fc0dde9274127b17f6a5
Harry Vu <harryvu801@gmail.com>
2017-08-23 18:20:10 -0600

    db connected

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   secret: 'keyboardcat',// Your secret here
   // connectstring: 'postgres://oebdqhdh:1A2Y_mAX5ZiIk4_tFUANeyZbthl2xEZ_@stampy.db.elephantsql.com:5432/oebdqhdh',
-  connectstring: "postgres://postgres@localhost/dominic_mac" ,
+  connectstring: "postgres://uunjpeyj:yVNsIpBpaTMB_a2TXEss-Gmq1DGSIOte@pellefant.db.elephantsql.com:5432/uunjpeyj" ,
   auth0: {
     domain: 'penguinhousedesigns.auth0.com',// App Auth0 domain,
     clientID: 'dQBbTjHyKT_VcOLxZ2tNCYTy7Gd5Pkyx',// Client ID,
